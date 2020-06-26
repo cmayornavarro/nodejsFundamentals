7ad29c41fa2c139cb5bbe5f6a0515a95f82889b0
cmayornavarro <cmayornavarro@gmail.com>
2020-06-26 06:44:56 +0200

    adding nodedb and 2 get api

--- app.js
+++ app.js
@@ -1,15 +1,48 @@
-const express = require('express');
-
+const express = require("express");
+const mongoose = require("mongoose");
+const db = mongoose.connect('mongodb://localhost/bookAPI');
 const app = express();
+const bookRouter = express.Router();
+const port = process.env.PORT || 3000;
+const Book = require('./models/bookModel');
 
-const port = process.env.PORT ||3000;
+bookRouter.route('/books')
 
-app.get('/', (req,res)=>{
+	.get((req,res)=> {
+		//const {query} = req;
+		const query = {};
+		if(req.query.genre){
+			query.genre = req.query.genre;
+		}
+		Book.find(query,(err,books)=>{
+			if(err){
+				return res.send(err);
+			}
+			return res.json(books);			
+		});
+		
+	});
+
+
+bookRouter.route('/books/:bookId')
+
+	.get((req,res)=> {
+
+		Book.findById(req.params.bookId,(err,book)=>{
+			if(err){
+				return res.send(err);
+			}
+			return res.json(book);			
+		});
+		
+	});
+
+app.use('/api',bookRouter); // for /api/books
+
+app.get("/", (req, res) => {
 	res.send("Welcome to my API");
 });
 
-
-
-app.listen(port,()=>{
+app.listen(port, () => {
 	console.log("running " + port);
 });
